b80672789255df5fbc1f67f04469d4e72b6457e2
Dias <dias.atudinov@gmail.com>
2025-12-02 00:58:47 +0500

    add All habits view

--- Simple Strength Habit/Simple Strength Habit/SHModules/SHAllHabits/SHAllHabitsView.swift
+++ Simple Strength Habit/Simple Strength Habit/SHModules/SHAllHabits/SHAllHabitsView.swift
@@ -2,17 +2,56 @@
 //  SHAllHabitsView.swift
 //  Simple Strength Habit
 //
-//  Created by Dias Atudinov on 01.12.2025.
 //
 
 import SwiftUI
 
 struct SHAllHabitsView: View {
+    @ObservedObject var viewModel: HabitViewModel
+    @State private var path: [Habit] = []
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack(path: $path) {
+            VStack(alignment: .leading, spacing: 0) {
+                Text("all habits")
+                    .font(.system(size: 25, weight: .black))
+                    .foregroundStyle(.text)
+                    .textCase(.uppercase)
+                
+                ScrollView(showsIndicators: false) {
+                    VStack(spacing: 21) {
+                        ForEach(viewModel.habits, id: \.self) { habit in
+                            SHHabitCell(habit: habit) {
+                                path.append(habit)
+                            }
+                        }
+                    }.padding(.top).padding(.bottom, 100)
+                }
+                .navigationDestination(for: Habit.self) { habit in
+                    //TODO: EditHabitView
+                    Text(habit.name.text)
+                        .font(.system(size: 35, weight: .bold))
+                }
+                
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(25)
+            .overlay(alignment: .bottomTrailing) {
+                Image(systemName: "plus")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 32)
+                    .bold()
+                    .foregroundStyle(.white)
+                    .padding(20)
+                    .background(.text)
+                    .clipShape(Circle())
+                    .padding(.trailing, 25)
+                    .padding(.bottom, 120)
+            }
+        }
     }
 }
 
 #Preview {
-    SHAllHabitsView()
+    SHAllHabitsView(viewModel: HabitViewModel())
 }
--- Simple Strength Habit/Simple Strength Habit/SHModules/SHAllHabits/Views/SHHabitCell.swift
+++ Simple Strength Habit/Simple Strength Habit/SHModules/SHAllHabits/Views/SHHabitCell.swift
@@ -2,17 +2,56 @@
 //  SHHabitCell.swift
 //  Simple Strength Habit
 //
-//  Created by Dias Atudinov on 01.12.2025.
 //
 
 import SwiftUI
 
 struct SHHabitCell: View {
+    @State var habit: Habit
+    var onEditPressed: () -> ()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack(alignment: .leading) {
+                Text(habit.name.text)
+                    .font(.system(size: 20, weight: .semibold))
+                    .foregroundStyle(.text)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text("\(habit.goal) \(habit.name.unitOfMeasurement.shortText)")
+                    .font(.system(size: 20, weight: .semibold))
+                    .foregroundStyle(.secondaryText)
+            }
+            
+            Toggle("", isOn: $habit.isCompleted)
+                .labelsHidden()
+                .tint(.text)
+                .allowsHitTesting(false)
+        }
+        .padding(.horizontal, 35)
+        .padding(.vertical, 13)
+        .background(.cellBg)
+        .clipShape(RoundedRectangle(cornerRadius: 15))
+        .overlay(alignment: .topLeading) {
+            Button {
+                onEditPressed()
+            } label: {
+                Image(systemName: "pencil")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(2)
+                    .frame(height: 24)
+                    .foregroundStyle(.white)
+                    .bold()
+                    .padding(4)
+                    .background(.text)
+                    .clipShape(Circle())
+                    .offset(y: -16)
+            }
+        }
     }
 }
 
 #Preview {
-    SHHabitCell()
+    SHHabitCell(habit:
+                    Habit(name: .waterIntake, goal: 2.5, progress: 0, type: .quantitative, isCompleted: true), onEditPressed: {}
+    )
 }
